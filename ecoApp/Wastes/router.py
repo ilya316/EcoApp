d3a429822817a950360263073ad9209e9cd1156f
Neitrem <a.v.gorshenin000@gmail.com>
2023-11-15 21:53:03 +0300

    f

--- ecoApp/Wastes/router.py
+++
@@ -1,6 +0,0 @@
-from rest_framework import routers
-
-from Wastes.views import WastSitesViewSet
-
-router = routers.SimpleRouter()
-router.register(r'wasts', WastSitesViewSet, basename="wastes")
--- ecoApp/Wastes/serializer.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-
-from Wastes.models import WasteSite
-
-class WastSirtesSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = WasteSite
-        fields = ['name', 'address', 'yard_type', 'lattitude', 'longitude']
