c0c6b6d25b86e8af0cf23a7b72a2b311f5f2d102
Neitrem <a.v.gorshenin003@gmail.com>
2023-11-24 10:20:15 +0300

    f

--- front/src/components/icons/IconProfile.vue
+++
@@ -1,7 +0,0 @@
-<template>
-    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path d="M12.1098 13C12.0457 12.9908 11.9634 12.9908 11.8902 13C10.2805 12.945 9 11.626 9 10.0046C9 8.34656 10.3354 7 12 7C13.6555 7 15 8.34656 15 10.0046C14.9909 11.626 13.7195 12.945 12.1098 13Z" stroke="#022601" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-        <path d="M18 18.6055C16.4154 20.0952 14.3145 21 12 21C9.68545 21 7.58457 20.0952 6 18.6055C6.08902 17.7464 6.62314 16.9056 7.57566 16.2475C10.0148 14.5842 14.003 14.5842 16.4243 16.2475C17.3769 16.9056 17.911 17.7464 18 18.6055Z" stroke="#022601" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-        <path d="M12 21C16.9705 21 21 16.9705 21 12C21 7.02943 16.9705 3 12 3C7.02943 3 3 7.02943 3 12C3 16.9705 7.02943 21 12 21Z" stroke="#022601" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-    </svg>
-</template>
